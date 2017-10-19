fca45d230921b57bd72fb083c300d8cb58ec76ee
Jamie White <jamie@jgwhite.co.uk>
2017-10-19 13:04:48 +0100

    Rebuild demo for 1.10.0

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/ember-sortable/demo/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
